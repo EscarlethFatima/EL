b7cb97322d805e4441117707e00845b4147cd10d
EscarlethFatima <Escarleth.Ledezma@fundacion-jala.org>
2018-07-30 17:23:01 +0000

    Gradle Practice

---
+++ quickstart/src/main/java/org/gradle/Person.java
@@ -0,0 +1,21 @@
+package org.gradle;
+
+import org.apache.commons.collections.list.GrowthList;
+
+public class Person {
+    private final String name;
+	private final int age;
+
+    public Person(String name) {
+        this.name = name;
+		this.age = 10;
+        new GrowthList();
+    }
+
+    public String getName() {
+        return name;
+    }
+	public int getAge() {
+        return age;
+    }
+}
