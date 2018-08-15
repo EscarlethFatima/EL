3fbbc670082c5569a7ebde089ce9262d3fa3a985
EscarlethFatima <Escarleth.Ledezma@fundacion-jala.org>
2018-08-15 22:05:42 +0000

    sh file updated-fixes2

--- webapp/deploy-app.sh
+++ webapp/deploy-app.sh
@@ -5,7 +5,7 @@ sudo rm -f /opt/tomcat/webapps/webapp.war
 sudo rm -rf /opt/tomcat/webapps/webapp
 
 echo 'Moving war file...'
-sudo cp -f /tmp/webapp.war /opt/tomcat/webapp.war
+sudo cp -f /tmp/webapp.war /opt/tomcat/webapps/webapp.war
 sudo rm /tmp/webapp.war
 
 echo 'Starting Tomcat service...'
