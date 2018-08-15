15d563c1ed511e2833210f4aa7d3b03c32bc9da4
EscarlethFatima <Escarleth.Ledezma@fundacion-jala.org>
2018-08-15 21:25:22 +0000

    sh file fixed

--- webapp/deploy-app.sh
+++ webapp/deploy-app.sh
@@ -1,12 +1,12 @@
 echo 'Stopping Tomcat service...'
 echo "${tomcat_webapps}/${war_name}.war"
 sudo systemctl stop tomcat
-sudo rm -f "${tomcat_webapps}/${war_name}.war"
-sudo rm -rf "${tomcat_webapps}/${war_name}""
+sudo rm -f ${tomcat_webapps}/${war_name}.war
+sudo rm -rf ${tomcat_webapps}/${war_name}
 
 echo 'Moving war file...'
-sudo cp -f /tmp/"${war_name}.war" "${tomcat_webapps}/${war_name}.war"
-sudo rm /tmp/"${war_name}.war"
+sudo cp -f /tmp/${war_name}.war ${tomcat_webapps}/${war_name}.war
+sudo rm /tmp/${war_name}.war
 
 echo 'Starting Tomcat service...'
 sudo systemctl start tomcat
